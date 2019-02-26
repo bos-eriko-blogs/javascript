04bbbdd56f22ab1330a2ac25323c9ddb06ca0a54
BosEriko <bos.eriko@gmail.com>
2019-02-26 18:50:35 +0800

    Renamed the author on gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Bos Eriko's JavaScript Blog`,
-    author: `Bos Eriko Reyes`,
+    author: `Bos Eriko`,
     description: `My random thoughts written in words.`,
     siteUrl: `https://js.boseriko.blog/`,
     social: {
